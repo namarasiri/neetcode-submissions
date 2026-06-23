1038affb5e6882b7be3846ee452e044380fc2f7d
Navin Amarasiri <50296136+namarasiri@users.noreply.github.com>
2026-06-24 07:03:41 +0930

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,42 @@
+class MinStack {
+
+    private Stack<Integer> minStack;
+    private Stack<Integer> temp = new Stack<>();
+
+    public MinStack() {
+        minStack = new Stack<>();
+        
+    }
+    
+    public void push(int val) {
+        minStack.push(val);
+    }
+    
+    public void pop() {
+        minStack.pop();
+    }
+    
+    public int top() {
+         return minStack.peek();
+    }
+    
+    public int getMin() {
+        
+        int min = minStack.peek();
+
+        while (!minStack.isEmpty()) {
+
+            min = Math.min(min, minStack.peek());
+            temp.push(minStack.pop());
+
+        }
+
+        while (!temp.isEmpty()) {
+
+            minStack.push(temp.pop());
+
+        }
+
+        return min;
+    }
+}
